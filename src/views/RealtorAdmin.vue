4921514d64a9fe1f69edb7280ec25f904e94e254
LEA THOMAS <LEATHOMASLT@GMAIL.COM>
2020-09-21 15:39:09 -0400

    added conditional messages for if a realtor has no listings or does have listings.

--- src/views/RealtorAdmin.vue
+++ src/views/RealtorAdmin.vue
@@ -2,17 +2,35 @@
 <div>
   <div class="admin-info-bar">
     <div>
-      <p>Active Listings: <span>{{realtorListings.length}}</span></p>
+      <p 
+      v-if="length">
+        Active Listings: 
+        <span>{{length}}</span>
+      </p>
     </div>
     <div class="admin-info-bar-functions">
       <p 
-        @click="isCreateListing">
-        New listing
+      class="create-link"
+      v-if="length"
+      @click="isCreateListing">
+        Publish new listing
       </p>
-      <p>
+      <p  v-if="length">
         Sort by: Newest
       </p>
     </div>
+  </div>
+  <div v-bind:class="{'is-no-listings': length}">
+      <p 
+      v-if="!length">
+        You have no active listings.
+      </p>
+      <p 
+      class="create-link"
+      v-if="!length"
+      @click="isCreateListing">
+        Publish a new listing here.
+      </p>
   </div>
   <Gridgallery 
   :isAdminPanel="isAdminPanel"
@@ -26,6 +44,8 @@
 
 <script>
 import Gridgallery from '../components/Gridgallery.vue'
+import Vue from 'vue'
+
 
 export default{
   name: "RealtorAdmin",
@@ -34,16 +54,25 @@ export default{
     Gridgallery,
   },
   props: ['loggedIn', 'isAdminPanel', 'realtorListings'],
+  // props: ['loggedIn', 'isAdminPanel',],
   data: function (){
     return {
-      houseData: this.checkRealtorListings()
+      // houseData: this.checkRealtorListings(),
+      length: this.getLength()
     }
   },
   methods:{
-    checkRealtorListings: function(){
+        getLength: function(){
       if(this.realtorListings){
-        this.houseData = this.realtorListings
+        return this.realtorListings.length
+      }else{
+        return false
       }
+    // },
+    // checkRealtorListings: function(){
+    //   if(this.realtorListings){
+    //     this.houseData = this.realtorListings
+    //   }
     },
     isCreateListing: function(){
       console.log('this is the create new listing trigger in editcreatelisting page')
@@ -67,8 +96,11 @@ export default{
               "Authorization" : `JWT ${this.$route.query.user.token}`
           }
       })
+      console.log('after delete listing ')
       // Emit to app.vue that a listing has been deleted to trigger a refresh of this.realtorListings prop.
       this.$emit('refreshRealtorListings', event)
+      console.log('after refresh listings ')
+      Vue.$forceUpdate();
     }
   }
 }
@@ -79,9 +111,9 @@ export default{
   display: flex;
   justify-content: space-between;
   align-items: flex-end;
-  padding-top: 100px;
   padding-bottom: 10px;
   width: 80%;
+  padding-top: 15%;
   margin: 0 auto;
 }
 .admin-info-bar p span{
@@ -103,6 +135,15 @@ export default{
 }
 .admin-info-bar-functions p:hover{
   opacity: .4;
+  cursor: pointer;
+}
+.if-no-listings{
+  text-align: center;
+  margin: 0 auto;
+  padding-top: 30%;
+}
+.create-link:hover{
+  opacity: .7;
   cursor: pointer;
 }
 </style>
